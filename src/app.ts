3eb7779e800662fc27ba148f013ce8baf65f7817
rasedprogrammer <rasedprogrammer@gmail.com>
2023-05-29 21:26:16 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-import express from "express";
-const app = express();
-export const port = 3000;
+import express, { Request } from 'express'
+import cors from 'cors'
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+const app = express()
 
-export default app;
+// Use Cors
+app.use(cors())
+// Use Parser
+app.use(express.json())
+// Use URL Encode
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res) => {
+  res.send('Hello World!')
+})
+
+export default app
