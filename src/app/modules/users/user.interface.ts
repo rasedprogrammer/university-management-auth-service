c529d90a5828d32abab162492e4ecd1381e3151a
rasedprogrammer <rasedprogrammer@gmail.com>
2023-06-20 23:41:06 +0600

    adding admin modules

--- src/app/modules/users/user.interface.ts
+++ src/app/modules/users/user.interface.ts
@@ -1,13 +1,15 @@
 import { Model, Types } from 'mongoose';
 import { IStudent } from '../student/student.interface';
+import { IFaculty } from '../faculty/faculty.interface';
+import { IAdmin } from '../admin/admin.interface';
 
 export type IUser = {
   id: string;
   role: string;
   password: string;
   student?: Types.ObjectId | IStudent;
-  // faculty?: Types.ObjectId | IFaculy;
-  // admin?: Types.ObjectId | IAdmin;
+  faculty?: Types.ObjectId | IFaculty;
+  admin?: Types.ObjectId | IAdmin;
 };
 
 // Create a new Model type that knows about IUserMethods...
