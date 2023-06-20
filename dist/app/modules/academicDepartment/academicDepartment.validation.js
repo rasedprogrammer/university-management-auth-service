eb664d72f747e7d02190d91f1c644a83e42843f7
rasedprogrammer <rasedprogrammer@gmail.com>
2023-06-20 23:23:00 +0600

    Adding management department Modules

--- dist/app/modules/academicDepartment/academicDepartment.validation.js
+++ dist/app/modules/academicDepartment/academicDepartment.validation.js
@@ -1,26 +1,26 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.AcademicDepartmentValidation = void 0;
-const zod_1 = require("zod");
+const zod_1 = require('zod');
 // Create Zod Schema
 const createDepartmentZodSchema = zod_1.z.object({
-    body: zod_1.z.object({
-        title: zod_1.z.string({
-            required_error: 'Title is Required.',
-        }),
-        academicFaculty: zod_1.z.string({
-            required_error: 'Academic Faculty is Required.',
-        }),
+  body: zod_1.z.object({
+    title: zod_1.z.string({
+      required_error: 'Title is Required.',
     }),
+    academicFaculty: zod_1.z.string({
+      required_error: 'Academic Faculty is Required.',
+    }),
+  }),
 });
 // Update Zod Schema
 const updateDepartmentZodSchema = zod_1.z.object({
-    body: zod_1.z.object({
-        title: zod_1.z.string().optional(),
-        academicFaculty: zod_1.z.string().optional(),
-    }),
+  body: zod_1.z.object({
+    title: zod_1.z.string().optional(),
+    academicFaculty: zod_1.z.string().optional(),
+  }),
 });
 exports.AcademicDepartmentValidation = {
-    createDepartmentZodSchema,
-    updateDepartmentZodSchema,
+  createDepartmentZodSchema,
+  updateDepartmentZodSchema,
 };
