f84c73ecb880b0ab2a587b00180964bd31c26b95
rasedprogrammer <rasedprogrammer@gmail.com>
2023-08-18 20:54:27 +0600

    [DONE]adding jwt access token and refresh token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,19 +3,48 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
 import { AuthService } from './auth.service';
+import { ILoginUserResponse, IRefreshTokenResponse } from './auth.interface';
+import config from '../../../config';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
   const result = await AuthService.loginUser(loginData);
+  const { refreshToken, ...others } = result;
 
-  sendResponse(res, {
+  // Set Refresh Token In Cookie
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+
+  sendResponse<ILoginUserResponse>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User Loggedin Successfully',
+    data: others,
+  });
+});
+
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
+  const { refreshToken } = req.cookies;
+  const result = await AuthService.refreshToken(refreshToken);
+
+  // Set Refresh Token In Cookie
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+
+  sendResponse<IRefreshTokenResponse>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User Loggedin Successfully',
     data: result,
   });
 });
-
 export const AuthController = {
   loginUser,
+  refreshToken,
 };
